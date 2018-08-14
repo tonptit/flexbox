7484cfc10c0e82034ba44ad4089947a148745888
Ton DZ <tontn@tinhvan.com>
2018-08-14 17:04:36 +0700

    init

---
+++ MultiAutoComplete/app/src/main/java/com/tvoer/multiautocomplete/MainActivity.java
@@ -0,0 +1,77 @@
+package com.tvoer.multiautocomplete;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.flexbox.FlexboxLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private FlexboxLayout flexBox;
+    private EditText edtName;
+    private Button btnAdd;
+    private Button btnDelete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        flexBox = findViewById(R.id.flexBox);
+        edtName = findViewById(R.id.edtName);
+        btnAdd = findViewById(R.id.add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewContact();
+            }
+        });
+
+
+        btnDelete = findViewById(R.id.delete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count = flexBox.getChildCount();
+                if (count - 2 >= 0) {
+                    flexBox.removeViewAt(count - 2);
+                }
+            }
+        });
+    }
+
+    private void addNewContact() {
+        FlexboxLayout.LayoutParams params1 = new FlexboxLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params1.setMargins(10, 10, 10, 10);
+        TextView tv1 = new TextView(this);
+        tv1.setText(edtName.getText());
+        tv1.setLayoutParams(params1);
+        tv1.setGravity(Gravity.CENTER);
+        tv1.setTextSize(20);
+        int count = flexBox.getChildCount();
+        flexBox.addView(tv1, count - 1);
+        edtName.setText("");
+        log();
+    }
+
+    private void log() {
+        View view;
+        for (int i = 0; i < flexBox.getChildCount(); i++) {
+            view = flexBox.getChildAt(i);
+
+            if (view instanceof EditText) {
+                Log.e("Flex", "EditText");
+
+            } else if (view instanceof TextView) {
+                Log.e("Flex", ((TextView) view).getText().toString());
+
+            }
+        }
+    }
+}
